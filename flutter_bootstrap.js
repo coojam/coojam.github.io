195cbdfd58548b39784c46a2c1f97d40315a5bb3
Matty Cooper <mattycoojam@gmail.com>
2025-06-15 16:38:54 +1000

    fix major performcne issue spamming backend

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1149701365"
+    serviceWorkerVersion: "3679178940"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c8bdbd9bba950fde1bc0105a2e122409",
+const RESOURCES = {"flutter_bootstrap.js": "ed786fc88f3876044f016bf75481e1df",
 "version.json": "3c4bbdca53af6faa85da704dae8a1e69",
 "index.html": "3888fe26f3da3f9b5d1466ca822aa930",
 "/": "3888fe26f3da3f9b5d1466ca822aa930",
-"main.dart.js": "eac91f4aff5b78dd1761a4f278fc8a0a",
+"main.dart.js": "c013d71c6d72498a654647d571e0cfeb",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
