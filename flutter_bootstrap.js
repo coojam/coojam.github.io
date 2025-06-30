0a838ba28ec32c174db19af1bdf1aa5dc7fc0923
Matty Cooper <mattycoojam@gmail.com>
2025-06-30 14:49:37 +1000

    0.0.19 countdown to sleep

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2764634951"
+    serviceWorkerVersion: "4233246474"
   }
 });
